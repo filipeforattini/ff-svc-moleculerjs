eb7ccdf0f5a571e641ab616dc371ff7d44a37d5c
Filipe Forattini <filipe@vitta.me>
2022-06-13 13:30:25 -0300

    fix: organized dependencies for dev environment

--- moleculer.config.js
+++ moleculer.config.js
@@ -1,19 +1,25 @@
 const { Middleware } = require("@moleculer/channels");
 
 const {
+  // generic config
   MOLECULER_CACHER,
   MOLECULER_TRANSPORTER,
   MOLECULER_REGISTRY,
   MOLECULER_CHANNEL,
+  // specific config
+  REDIS_CONNECTION_STRING,
+  NATS_CONNECTION_STRING,
+  ETCD_CONNECTION_STRING,
+  RABBITMQ_CONNECTION_STRING,
 } = process.env;
 
 module.exports = {
-  cacher: MOLECULER_CACHER,
-  transporter: MOLECULER_TRANSPORTER,
+  cacher: REDIS_CONNECTION_STRING || MOLECULER_CACHER,
+  transporter: NATS_CONNECTION_STRING || MOLECULER_TRANSPORTER,
   registry: {
-    discoverer: MOLECULER_REGISTRY,
+    discoverer: ETCD_CONNECTION_STRING || MOLECULER_REGISTRY,
   },
   middlewares: [
-    Middleware({ adapter: MOLECULER_CHANNEL }),
+    Middleware({ adapter: RABBITMQ_CONNECTION_STRING || REDIS_CONNECTION_STRING || MOLECULER_CHANNEL }),
   ],
 };
